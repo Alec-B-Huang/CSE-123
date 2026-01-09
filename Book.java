fbf17f31b0f675f00710e6856e1a05f7299de82c
Alec-B-Huang <67602734+Alec-B-Huang@users.noreply.github.com>
2026-01-08 21:24:30 -0800

    Add files via upload
    Book File outline for C0

---
+++ Book.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Book implements Media{
+    private String title;
+    private List<String> authors;
+    private List<String> content;
+
+    public Book(String title, List<String> authors, Scanner content) {
+        this.content = new ArrayList<>();
+    }
+
+    @Override
+    public String getTitle() {
+
+    }
+
+    @Override
+    public List<String> getArtists() {
+
+    }
+
+    @Override
+    public void addRating(int score) {
+
+    }
+
+    @Override
+    public int getNumRatings() {
+
+    }
+
+    @Override
+    public double getAverageRating() {
+
+    }
+
+    @Override
+    public List<String> getContent() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+    
+}
