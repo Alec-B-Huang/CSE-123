a988cc00bc5b2aab0a2fe40afcf9fd57dfce9d81
Alec-B-Huang <67602734+Alec-B-Huang@users.noreply.github.com>
2026-01-19 21:11:41 -0800

    Update CaesarKey.java
    Added comments

--- CaesarKey.java
+++ CaesarKey.java
@@ -1,16 +1,30 @@
 // Alec Huang
+// 01/19/26
 // CSE 123 
 // P0
-// TA: Ishita
+// TA: Ishita Mundra
 
 import java.util.*;
 
+// Class comment: Represents CaesarKey cipher by making a substitution
+// cipher by placing key at front of encoding then putting remaining characters
+// in order after key.
 public class CaesarKey extends Substitution {
 
+    // BEHAVIOR: constructs CaesarKey cipher
+    // EXCEPTIONS: none 
+    // RETURNS: none
+    // PARAMETERS: String key used to make substitution encoding
     public CaesarKey(String key) {
         super(buildEncoding(key));
     }
 
+    // BEHAVIOR: Builds and returns encoding
+    // EXCEPTIONS: IllegalArgumentException thrown if key is null
+    //             if character doesn't exist n VALID_CHARS
+    //             if duplicate letters in key
+    // RETURNS: String encoding which is the encrypted string
+    // PARAMETERS: String key used to make substitution encoding
     private static String buildEncoding(String key) {
         if (key == null) {
             throw new IllegalArgumentException();
