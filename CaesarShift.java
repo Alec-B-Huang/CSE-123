15f3bfa0a090a39955dee33c7fcbe10a4738d3d6
Alec-B-Huang <67602734+Alec-B-Huang@users.noreply.github.com>
2026-01-19 21:11:55 -0800

    Update CaesarShift.java
    + comments

--- CaesarShift.java
+++ CaesarShift.java
@@ -1,15 +1,30 @@
 // Alec Huang
+// 01/19/26
 // CSE 123 
 // P0
-// TA: Ishita
+// TA: Ishita Mundra
 
+// Class comment: Represents a CaesarShift cipher that shifts each character
+// a fixed number of places to the left.
 public class CaesarShift extends Substitution {
 
+    // BEHAVIOR: Constructs CaesarShift cipher.
+    // EXCEPTIONS: none
+    // RETURNS: none 
+    // PARAMETERS: Int shift is the number of places to shift 
+    //             characters in the phrase over by.
     public CaesarShift(int shift) {
         super(buildEncoding(shift));
     }
 
-    public static String buildEncoding(int shift) {
+    // BEHAVIOR: Builds encoded string by moving certain characters
+    // set amount to the left.
+    // EXCEPTIONS: Illegal Argument Exception if shift is 
+    //             less than one.
+    // RETURNS: String result which is the encrypted string
+    // PARAMETERS: Int shift is the number of places to shift 
+    //             characters in the phrase over by.
+    private static String buildEncoding(int shift) {
         if (shift < 0) {
             throw new IllegalArgumentException();
         }
