054e3b99d342932f637d1cf8d58c98b034de2368
Alec-B-Huang <67602734+Alec-B-Huang@users.noreply.github.com>
2026-02-13 18:12:28 -0800

    Update Client.java
    Updated with private and public divide methods to match spec signature

--- Client.java
+++ Client.java
@@ -6,15 +6,24 @@ public class Client {
         Picture picture = new Picture(400, 400);
         Color[][] pixels = picture.getPixels();
         // 2. Call divide divide canvas
-        divideCanvas(pixels, 2, 0, 400, 0, 400);
+        divideCanvas(pixels, 2);
         // 3. Save the image to display it
-        picture.save("picture.jpg");
         picture.setPixels(pixels);
+        picture.save("picture.jpg");
     }
 
     // TODO: Implement divideCanvas below using your 'fill' implementation
     // from the previous slide
-    public static void divideCanvas(Color[][] pixels, int n, int x1, int x2, int y1, int y2) {
+    public static void divideCanvas(Color[][] pixels, int n) {
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = pixels[0].length;
+        int y2 = pixels.length;
+
+        divideCanvas(pixels, n, x1, x2, y1, y2);
+    }
+
+    private static void divideCanvas(Color[][] pixels, int n, int x1, int x2, int y1, int y2) {
         if (n == 0) {
             fillRegion(pixels, x1, x2, y1, y2);
             return;
@@ -33,8 +42,7 @@ public class Client {
     public static void fillRegion(Color[][] pixels, int x1, int x2, int y1, int y2) {
         for (int i = (y1 + 1); i < (y2 - 1); i++) {
             for (int j = (x1 + 1); j < (x2 - 1); j++) {
-                Color white = new Color(255, 255, 255);
-                pixels[i][j] = white;
+                pixels[i][j] = Color.WHITE;
             }
         }
     }
